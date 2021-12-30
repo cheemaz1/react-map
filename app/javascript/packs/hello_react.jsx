1fe508324664fc07d144515c2b77b2ee03c9d09a
cheemaz1 <zayedcheema@hotmail.com>
2021-12-30 16:43:42 -0500

    fixed some styling

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -11,14 +11,21 @@ const instruction = []
 const distonce = []
 const total_time = []
 const total_distance = []
+const lng = []
+const ltd = []
+const longitude2 = []
+const latitude2 = []
+const destination = []
 
 const Hello = props => {
+  debugger;
   const directionsService = new google.maps.DirectionsService();
   const value = []
   const address = props.appointments.address
-  const destination = props.appointments.destination
-
-
+  destination.push(props.appointments.destination)
+  lng.push(props.appointments.longitude)
+  ltd.push(props.appointments.latitude)
+  
   // ${ document.getElementById("myInput").value }
   // debugger;
   directionsService.route(
@@ -59,21 +66,20 @@ const Hello = props => {
   )
 }
 const Mapp = props => {
+  
   for (let z = 0; z < instruction.length; z++) {
     // var elem = document.createElement('li');
-    var elem = document.body.appendChild(document.createElement('ul'));
+    var elem = document.body.appendChild(document.createElement('li'));
     elem.innerHTML = instruction[z] + " for " + distonce[z].text;
   }
   return (
     <div>
   
-        <div>Total  Distance of the trip: {total_distance[0]}</div>
+        <div className="duration">Total  Distance of the trip: {total_distance[0]}</div>
         <div className="duration">Total  Duration of the trip: {total_time[0]}</div>
     </div>
   )
 }
-
-
 
 
 document.addEventListener('DOMContentLoaded', () => {
